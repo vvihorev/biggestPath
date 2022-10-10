7106267a2a585889b270628531afbd1e434d7e73
vvihorev <vvihorevv@gmail.com>
2022-10-11 00:13:19 +0200

    added comments for clarity

--- main.py
+++ main.py
@@ -5,23 +5,22 @@ def biggestPath(X: dict) -> str:
     def bP(cur: dict | list, pwd: str) -> str:
         if len(pwd) > 255 or re.search(r"[^0-9a-zA-Z/]", pwd) is not None:
             return "/"
-        if cur == {}:
-            return pwd
+        # if cur is a list
         if type(cur) == list:
-            if len(set(cur)) < len(cur):
-                return "/"
             files = [file for file in cur if re.search(r"[^0-9a-zA-Z/]", file) is None]
-            if len(files) == 0:
+            if len(files) == 0 or len(set(cur)) < len(cur):
                 return "/"
             file = min(files, key=(lambda file: len(file)))
             if len(pwd + file) > 255:
                 return "/"
             return pwd + "/" + file
+        # if cur is a dict
+        if cur == {}:
+            return pwd
         if len(set(cur.keys())) < len(cur.keys()):
             return "/"
         return max(
             [bP(contents, pwd + "/" + subdir) for subdir, contents in cur.items()],
             key=(lambda x: len(x.split("/"))),
         )
-
     return bP(X, "")
